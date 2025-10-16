85cd307765d7a623934ad653c3c52d66b3d16d90
RiceCell <russel.me789@gmail.com>
2025-10-17 00:34:58 +0800

    Installed Tailwind CSS in frontend setup ONLY. Please see TailwindTest.vue and App.vue for difference as well.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TailwindTest from './components/TailwindTest.vue'
 import { ref, onMounted } from 'vue'
 
 const message = ref("Loading...")
@@ -13,30 +12,12 @@ onMounted(async () => {
 
 </script>
 
-<template>
-  <h1>{{ message }}</h1>
+<template> 
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <TailwindTest></TailwindTest>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ frontend/src/components/TailwindTest.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class = "text-4xl text-blue-500">
+        Hello Tailwind! This confirms that Tailwind is here. YIPEE!
+    </div>
+</template>
