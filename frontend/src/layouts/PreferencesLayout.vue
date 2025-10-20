065e277af316510ae0bbc18cdf5013eafa6e9045
DareDevilLuc <kscasirayan@up.edu.ph>
2025-10-20 15:32:08 +0800

    Add Preferences layout and Select Preferences view; update router redirection

---
+++ frontend/src/layouts/PreferencesLayout.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex justify-center items-center min-h-screen">
+        <div class="bg-white max-w-[1053px] w-[1053px] max-h-[817px] h-[817px] rounded-[100px] text-center">
+        </div>
+    </div>
+</template>
